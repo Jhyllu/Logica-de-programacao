4270bf8eeb1974bfee19b0b04ef3786f8a0583b4
Jhyllu <98857564+Jhyllu@users.noreply.github.com>
2024-03-15 21:44:17 -0300

    Update exe09.c

--- exe09.c
+++ exe09.c
@@ -7,14 +7,16 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 
 #include <stdio.h>
+
 #define PORCENT 0.10
 
 int main()
 {
-    float valorGasto, gorjeta;
+    float valorGasto, gorjeta, total;
     printf("Informe o valor gasto: ");
     scanf("%f", &valorGasto);
     
-    gorjeta = valorGasto * porcent;
-    printf("O valor da gorjeta é: %.2f", gorjeta);
+    gorjeta = valorGasto * PORCENT;
+    total = valorGasto + gorjeta;
+    printf("O valor total é %.2f, com uma gorjeta de %.2f", total, gorjeta);
 }
