e987006e07c92dd79cf9e04f45a64bcf6fd8c4cf
Jhyllu <98857564+Jhyllu@users.noreply.github.com>
2024-03-18 21:30:58 -0300

    Update exe05-1803.c

--- exe05-1803.c
+++ exe05-1803.c
@@ -7,20 +7,19 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 
 #include <stdio.h>
-#include <math.h>
 
 int main()
 {
-    float largura, altura, area, potencia;
+    float largura, profundidade, area, potencia;
     
     printf("Insira a largura do cômodo: ");
     scanf("%f", &largura);
     
-    printf("Insira a altura: ");
-    scanf("%f", &altura);
+    printf("Insira a profundidade: ");
+    scanf("%f", &profundidade);
     
-    area = largura * altura;
-    potencia = (largura * altura) * 18;
+    area = largura * profundidade;
+    potencia = (largura * profundidade) * 18;
     
     printf("Na area de %.2f, é necessário %.2fW de potencia", area, potencia);
 
